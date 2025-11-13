7b8b2b0af84762b15e456b3edaef2448fa20ebe1
Daniel Nova <d.novap@uniandes.edu.co>
2025-11-13 10:17:28 -0500

    digraph

---
+++ DataStructures/Graph/digraph.py
@@ -0,0 +1,19 @@
+from DataStructures.Map import map_separate_chaining as sc
+
+def new_graph(order):
+    pass
+
+def insert_vertex(my_graph, key_u, info_u):
+    pass
+
+def add_edge(my_graph, key_u, key_v, weight=1.0):
+    pass
+
+def contains_vertex(my_graph, key_u):
+    pass
+
+def order(my_graph):
+    pass
+
+def size(my_graph):
+    pass
