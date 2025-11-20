5d298c8fdbf4cdbbfc56b553825341a6e46c0d16
SantiagoPinzon26 <S.pinzonc2@uniandes.edu.co>
2025-11-20 10:28:24 -0500

    bfs

--- DataStructures/Graph/bfs.py
+++ DataStructures/Graph/bfs.py
@@ -1,17 +1,48 @@
 from DataStructures.Map import map_linear_probing as mp
 from DataStructures.Graph import digraph as dig
-from DataStructures.Graph import dfo_structure as dfo
+from DataStructures.Graph import dfo_structure as dfo   
 from DataStructures.Queue import queue as q
 from DataStructures.Stack import stack as st
 from DataStructures.List import array_list as lt
 
 
 def bfs(my_graph, source):
-    pass
+    if my_graph is None:
+        return None
+
+    vertex_source = mp.get(my_graph["vertices"], source)
+    if vertex_source is None:
+        raise Exception("El vertice source no existe")
+
+    visited_map = {
+        "marked": mp.new_map(dig.order(my_graph), 0.5),
+        "edge_to": mp.new_map(dig.order(my_graph), 0.5),
+        "queue": q.new_queue(),
+        "source": source
+    }
+
+    bfs_vertex(my_graph, vertex_source, visited_map)
+    return visited_map
 
 
 def bfs_vertex(my_graph, vertex, visited_map):
-    pass
+    marked = visited_map["marked"]
+    edge_to = visited_map["edge_to"]
+    queue = visited_map["queue"]
+
+    mp.put(marked, vertex["key"], True)
+    q.enqueue(queue, vertex)
+
+    while not q.is_empty(queue):
+        current_vertex = q.dequeue(queue)
+        adjacents = current_vertex.get("adjacents", None)
+        if adjacents is not None:
+            for key_v in mp.keys(adjacents):
+                if not mp.contains(marked, key_v):
+                    mp.put(marked, key_v, True)
+                    mp.put(edge_to, key_v, current_vertex["key"])
+                    neighbor_vertex = mp.get(my_graph["vertices"], key_v)
+                    q.enqueue(queue, neighbor_vertex)
 
 
 def has_path_to(key_v, visited_map):
